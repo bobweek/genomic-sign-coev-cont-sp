0baaa1ed6453b67e7e1b1b6238a231a62f9d4d87
Bob Week <bobweek@gmail.com>
2022-05-09 08:00:08 -0400

    stable-dynamics

--- rasts.sh
+++ rasts.sh
@@ -4,8 +4,8 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate slim-tskit
 python python/rasts.py
 R -f r/rasts.R
-cd ~/gsccs-data/rasts-data/
-ffmpeg -r 20 -i N%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p N.mp4
-ffmpeg -r 20 -i z%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p z.mp4
-ffmpeg -r 20 -i v%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p v.mp4
-ffmpeg -r 20 -i Nz%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p Nz.mp4
+cd ~/gsccs-data/rast-data/
+ffmpeg -r 20 -i N%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p N.mp4 -y
+ffmpeg -r 20 -i z%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p z.mp4 -y
+ffmpeg -r 20 -i v%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p v.mp4 -y
+ffmpeg -r 20 -i Nz%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p Nz.mp4 -y
