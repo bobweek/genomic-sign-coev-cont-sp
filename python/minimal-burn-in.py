0889443f9c14519be363c64cae418a8ba6641a37
Bob Week <bobweek@gmail.com>
2022-05-11 11:42:23 -0400

    slimuations slimulating more slim-like

--- python/minimal-burn-in.py
+++ python/minimal-burn-in.py
@@ -4,31 +4,22 @@ import pyslim
 import numpy as np
 import os
 
-N = 10000
-n = 1000
+N = 1000
+n = 100
 L = 1e8
 mu = 1e-13
-k = 1
+k = 1 # size of gaussian mutations
 rho = 1e-8
 
-# first simulate whole genome to get num segr sites
-# to get freely recomb loci, then run a sim per segr site
-
-# generate genealogy
-# demog_model = msprime.Demography() # this was producing two populations per species
-# demog_model.add_population(initial_size=N)
-# hts = msprime.sim_ancestry(samples=n,demography=demog_model,sequence_length=L,recombination_rate=rho)
-# pts = msprime.sim_ancestry(samples=n,demography=demog_model,sequence_length=L,recombination_rate=rho)
 hts = msprime.sim_ancestry(samples=n,population_size=N,sequence_length=L,recombination_rate=rho)
 pts = msprime.sim_ancestry(samples=n,population_size=N,sequence_length=L,recombination_rate=rho)
 
-# annotate
 hts = pyslim.annotate_defaults(hts, model_type="nonWF", slim_generation=1)
 pts = pyslim.annotate_defaults(pts, model_type="nonWF", slim_generation=1)
 
 # slim mutations
 hmut = msprime.SLiMMutationModel(type=1) # type = 1 for m1 (spp 1)
-pmut = msprime.SLiMMutationModel(type=2) # type = 1 for m1 (spp 1)
+pmut = msprime.SLiMMutationModel(type=2) # type = 2 for m2 (spp 2)
 hts = msprime.sim_mutations(hts,rate=mu,model=hmut,keep=True)
 pts = msprime.sim_mutations(pts,rate=mu,model=pmut,keep=True)
 print(f"The host now has {hts.num_mutations} mutations, at "f"{hts.num_sites} distinct sites.")
@@ -68,36 +59,29 @@ for m in pts.mutations():
           m.replace(metadata={"mutation_list": md_list})
   )
 
-# check we didn't mess anything up
-assert htables.mutations.num_rows == hts.num_mutations
-assert ptables.mutations.num_rows == pts.num_mutations
-print(f"The host trait effects range from {min(hmut.values()):0.2e}"f" to {max(hmut.values()):0.2e}.")
-print(f"The para trait effects range from {min(pmut.values()):0.2e}"f" to {max(pmut.values()):0.2e}.")
+#
+# editing spp1 metadata
+#
 
-# what kind slim we runnin here??
-htables.metadata
-ptables.metadata
-
-# gotta change a few things
 ts_metadata = htables.metadata
 ts_metadata["SLiM"]["spatial_dimensionality"] = "xy"
 ts_metadata['SLiM']['cycle'] = 1
 ts_metadata['SLiM']['tick'] = 1
 htables.metadata = ts_metadata
 
-hindividual_metadata = [ind.metadata for ind in htables.individuals]
-for md in hindividual_metadata:
-   md["subpopulation"] = 0
-   ims = htables.individuals.metadata_schema
-   htables.individuals.packset_metadata(
-      [ims.validate_and_encode_row(md) for md in hindividual_metadata])
+# hindividual_metadata = [ind.metadata for ind in htables.individuals]
+# for md in hindividual_metadata:
+#    md["subpopulation"] = 0
+#    ims = htables.individuals.metadata_schema
+#    htables.individuals.packset_metadata(
+#       [ims.validate_and_encode_row(md) for md in hindividual_metadata])
 
-hmut_metadata = [mut.metadata for mut in htables.mutations]
-for md in hmut_metadata:
-   md["mutation_list"][0]["subpopulation"] = 0
-   ims = htables.mutations.metadata_schema
-   htables.mutations.packset_metadata(
-      [ims.validate_and_encode_row(md) for md in hmut_metadata])
+# hmut_metadata = [mut.metadata for mut in htables.mutations]
+# for md in hmut_metadata:
+#    md["mutation_list"][0]["subpopulation"] = 0
+#    ims = htables.mutations.metadata_schema
+#    htables.mutations.packset_metadata(
+#       [ims.validate_and_encode_row(md) for md in hmut_metadata])
 
 htables.populations.clear()
 for p in hts.populations():
@@ -109,25 +93,29 @@ for p in hts.populations():
 hts = htables.tree_sequence()
 hts.dump(os.path.expanduser('~/gsccs-data/hinit.trees'))
 
+#
+# editing spp1 metadata
+#
+
 ts_metadata = ptables.metadata
 ts_metadata["SLiM"]["spatial_dimensionality"] = "xy"
-ts_metadata['SLiM']['cycle'] = 1
-ts_metadata['SLiM']['tick'] = 1
+# ts_metadata['SLiM']['cycle'] = 1
+# ts_metadata['SLiM']['tick'] = 1
 ptables.metadata = ts_metadata
 
-pindividual_metadata = [ind.metadata for ind in ptables.individuals]
-for md in pindividual_metadata:
-   md["subpopulation"] = 1
-   ims = ptables.individuals.metadata_schema
-   ptables.individuals.packset_metadata(
-      [ims.validate_and_encode_row(md) for md in pindividual_metadata])
+# pindividual_metadata = [ind.metadata for ind in ptables.individuals]
+# for md in pindividual_metadata:
+#    md["subpopulation"] = 1
+#    ims = ptables.individuals.metadata_schema
+#    ptables.individuals.packset_metadata(
+#       [ims.validate_and_encode_row(md) for md in pindividual_metadata])
 
-pmut_metadata = [mut.metadata for mut in ptables.mutations]
-for md in pmut_metadata:
-   md["mutation_list"][0]["subpopulation"] = 1
-   ims = ptables.mutations.metadata_schema
-   ptables.mutations.packset_metadata(
-      [ims.validate_and_encode_row(md) for md in pmut_metadata])
+# pmut_metadata = [mut.metadata for mut in ptables.mutations]
+# for md in pmut_metadata:
+#    md["mutation_list"][0]["subpopulation"] = 1
+#    ims = ptables.mutations.metadata_schema
+#    ptables.mutations.packset_metadata(
+#       [ims.validate_and_encode_row(md) for md in pmut_metadata])
 
 ptables.populations.clear()
 ptables.populations.add_row()
@@ -139,15 +127,3 @@ for p in pts.populations(): # make empty pop in pop[0]
     ptables.populations.add_row(metadata=pm)
 pts = ptables.tree_sequence()
 pts.dump(os.path.expanduser('~/gsccs-data/pinit.trees'))
-
-# trait values
-# Zh = np.zeros(n)
-# Zp = np.zeros(n)
-# hgm = hts.genotype_matrix()
-# pgm = pts.genotype_matrix()
-# for i in 2*np.arange(n):
-#     Zh[int(i/2)] = sum(hgm[:,i]*hfx)+sum(hgm[:,i+1]*hfx)
-#     Zp[int(i/2)] = sum(pgm[:,i]*pfx)+sum(pgm[:,i+1]*pfx)
-
-# np.var(Zh,ddof=1)
-# np.var(Zp,ddof=1)
