321cd95ce0a3ec8290a3cb1a32ef6fc715b5d27b
Bob Week <bobweek@gmail.com>
2022-05-11 20:45:46 -0400

    moretify!

--- rasts.sh
+++ rasts.sh
@@ -9,3 +9,5 @@ ffmpeg -r 20 -i N%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p N.mp4 -y
 ffmpeg -r 20 -i z%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p z.mp4 -y
 ffmpeg -r 20 -i v%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p v.mp4 -y
 ffmpeg -r 20 -i Nz%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p Nz.mp4 -y
+
+ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'rasters complete!'
--- time-series.sh
+++ time-series.sh
@@ -4,3 +4,5 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate slim-tskit
 python python/slimulation-analysis.py
 R -f r/visualize-time-series.R
+
+ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'slimulation analysis complete!'
