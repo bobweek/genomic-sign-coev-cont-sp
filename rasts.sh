311bb985c670971ed89163c716eab26755e086a1
Bob Week <bobweek@gmail.com>
2022-05-12 13:54:44 -0400

    remove old data before compiling new data

--- rasts.sh
+++ rasts.sh
@@ -2,9 +2,12 @@
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate slim-tskit
+rm ~/gsccs-data/rast-data/*.csv
 python python/rasts.py
+rm ~/gsccs-data/rast-data/*.png
 R -f r/rasts.R
 cd ~/gsccs-data/rast-data/
+rm *.mp4
 ffmpeg -r 20 -i N%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p N.mp4 -y
 ffmpeg -r 20 -i z%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p z.mp4 -y
 ffmpeg -r 20 -i v%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p v.mp4 -y
--- slim-rast.sh
+++ slim-rast.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm ~/gsccs-data/ind-data/*.csv
 slim slim/para-host.slim
 ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'slimulation complete!'
 ./rasts.sh
