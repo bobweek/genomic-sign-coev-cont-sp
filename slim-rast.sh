0f18e16ea29de79ee66a887ed244f5dbc90737e8
Bob Week <bobweek@gmail.com>
2022-05-11 20:43:18 -0400

    notify!

--- slim-rast.sh
+++ slim-rast.sh
@@ -2,3 +2,5 @@
 
 slim slim/multispp-slim/para-host.slim
 ./rasts.sh
+
+ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'slimulation rasters complete!'
--- slim-rasts-time.sh
+++ slim-rasts-time.sh
@@ -3,3 +3,5 @@
 slim slim/multispp-slim/para-host.slim
 ./rasts.sh
 ./time-series.sh
+
+ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'slimulation rasters and analysis complete!'
