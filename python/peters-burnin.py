063b225368c6167f09efb1fae645e5ab5e8d299e
Bob Week <bobweek@gmail.com>
2022-05-16 18:08:28 -0400

    ironedoutsomekinks

---
+++ python/peters-burnin.py
@@ -0,0 +1,27 @@
+import msprime, pyslim
+import os
+
+def add_tick_cycle(ts):
+    tables = ts.dump_tables()
+    ts_metadata = tables.metadata
+    ts_metadata['SLiM']['cycle'] = 1
+    ts_metadata['SLiM']['tick'] = 1
+    tables.metadata = ts_metadata
+    return tables.tree_sequence()
+
+hts = msprime.sim_ancestry(samples=10, population_size=10, sequence_length=10)
+hts = pyslim.annotate_defaults(hts, model_type="nonWF", slim_generation=1)
+hts = add_tick_cycle(hts)
+
+hts.dump(os.path.expanduser('~/gsccs-data/hinit.trees'))
+
+# here pop_0 will be an unsampled population that doesn't affect anything
+demog = msprime.Demography.from_species_tree(
+        "(pop_0:100000000.0,pop_1:100000000.0)A",
+        initial_size={"pop_0": 10, "pop_1": 10, "A": 10}
+)
+
+pts = msprime.sim_ancestry(samples={"pop_0": 0, "pop_1": 10}, demography=demog, sequence_length=20)
+# assert np.all(pts.tables.nodes.population == 1)
+pts = pyslim.annotate_defaults(pts, model_type="nonWF", slim_generation=1)
+pts = add_tick_cycle(pts)
--- r/pt-plts.R
+++ r/pt-plts.R
@@ -6,6 +6,10 @@ mmdf = read.csv("~/gsccs-data/minmax.csv")
 
 ndatpts = length(list.files(path = "~/gsccs-data/ind-data/", pattern = "csv"))
 
+mmdf = read.csv("~/gsccs-data/minmax.csv")
+mnn = min(mmdf$min)
+mxx = max(mmdf$max)
+
 for(i in 1:ndatpts){
   num = sprintf("%04d", i)
   datfile = paste("~/gsccs-data/ind-data/indData",num,".csv",sep="")
@@ -14,8 +18,7 @@ for(i in 1:ndatpts){
   pz <- ggplot(inddf) +
     geom_point(aes(color=z,x=x,y=y),alpha=0.5,stroke=0.75,size=0.5) +
     facet_grid(.~spp) +
-    scale_colour_viridis() +
-    # scale_colour_viridis(limits=c(mmdf$min,mmdf$max)) +
+    scale_colour_viridis(limits=c(mnn,mxx)) +
     dark_theme_gray()
   pzf = paste("~/gsccs-data/ind-data/z",num,".png",sep="")
   ggsave(pzf,pz,width=16,height=8,bg="black")
