f23c5943c5fc448135a37dfcac42fea9731bf0e5
Bob Week <bobweek@gmail.com>
2022-05-24 12:13:28 -0400

    noconda

--- slim-time-series.sh
+++ slim-time-series.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm ~/gsccs-data/ind-data/*.csv
-conda activate slim-tskit
 python python/burn-in.py
 slim slim/mortal-para-host.slim
 ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'slimulation complete!'
