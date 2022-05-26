eff435ede8c8b1d8dfa8afd2e2f77d57dc124dd0
Bob Week <bobweek@gmail.com>
2022-05-26 19:43:42 -0400

    replicatingreplicatesreplicate

--- replicates.sh
+++ replicates.sh
@@ -2,4 +2,4 @@
 
 cd peqg22
 python replicates.py
-# ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'replicates finished!'
+ntfy -b pushover -o user_key uagcx5q4jqpv21t1mowsp5dfecjut8 send 'replicates finished!'
